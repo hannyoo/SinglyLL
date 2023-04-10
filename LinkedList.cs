7e89aedb800511206b00becb5117acede9fc70f7
hannyoo <119824566+hannyoo@users.noreply.github.com>
2023-04-11 01:28:22 +0900

    LL

---
+++ LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace SinglyLinkedList
+{
+	public class LinkedList
+	{
+		public NodeClass headNode;
+
+		public LinkedList()
+		{
+			headNode = null;
+		}
+
+		public void AddHead( int data )
+		{
+			NodeClass newNode = new NodeClass(data);
+			newNode.next = headNode;
+			headNode = newNode;
+		}
+
+		public void AddTail(int data)
+		{
+			NodeClass newNode = new NodeClass(data);
+
+			if(headNode == null)
+			{
+				headNode = newNode;
+			}
+			else
+			{
+				NodeClass curNode = newNode;
+				while(curNode.next != null)
+				{
+					curNode = curNode.next;
+				}
+				curNode.next = newNode;
+			}
+		}
+
+		public bool RemoveData(int data)
+		{
+			if(headNode == null)
+				return false;
+
+			if(headNode.data == data)
+			{
+				headNode = headNode.next;
+				return true;
+			}
+
+			NodeClass curNode = headNode;
+
+			while(curNode.data == data)
+			{
+				return true;
+
+				if(curNode.next.data == data)
+				{
+					curNode.next.data = curNode.next.next.data;
+                }
+                curNode = curNode.next;
+            }
+				return false;
+		}
+
+        public NodeClass AddIndex( int index )
+        {
+			if(index < 0)
+			{
+                return null;
+			}
+
+			NodeClass curNode = headNode;
+
+			for(int i = 0; i < index && curNode != null; i++)
+            {
+                curNode = curNode.next;
+            }
+            return curNode;
+        }
+
+		public NodeClass GetIndexData(int index)
+		{
+			if(index<0)
+			{
+				return null;
+			}
+			NodeClass curNode = headNode;
+
+
+		}
+
+
+    }
+}
+
---
+++ NodeClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SinglyLinkedList
+{
+	public class NodeClass
+	{
+		public int data;
+		public NodeClass next;
+
+		public NodeClass( int data_ )
+		{
+			data = data_;
+			next = null;
+		}
+	}
+
+}
+
